219ff85faaf581c046584f0b642bf5eeebf5d953
Veronica <vicareon@gmail.com>
2022-09-01 10:35:21 -0300

    Java Cars, Assemble! exercise

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,19 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        if(speed >= 0 && speed <= 4){
+            return (speed * 221);
+        }
+        if(speed > 4 && speed <= 8){
+            return (speed * 221) * 0.9;
+        }
+        if(speed == 9){
+            return (speed * 221) * 0.8;
+        }
+        return (speed * 221) * 0.77;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed) / 60;
     }
 }
