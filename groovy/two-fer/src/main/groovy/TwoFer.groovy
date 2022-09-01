e6d7d6a8501335b3e75d8ec2d911207cbf406096
Veronica <vicareon@gmail.com>
2022-09-01 09:19:06 -0300

    Groovy TwoFer exercise

--- groovy/two-fer/src/main/groovy/TwoFer.groovy
+++ groovy/two-fer/src/main/groovy/TwoFer.groovy
@@ -1,18 +1,8 @@
 class TwoFer {
-
     static String twoFer(String name) {
-<<<<<<< Updated upstream
-        if(name == null || name.isEmpty()){
-            return println('One for you, one for me.')
-        }
-        return println('One for ' + name + ', one for me.')
-    }
-}
-=======
         if(!name){
             return "One for you, one for me."
         }
         return "One for ${name}, one for me."
     }
 }
->>>>>>> Stashed changes
