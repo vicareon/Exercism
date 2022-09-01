617e07b0a1474fab835ba7691c8eeff5882056cc
Veronica <vicareon@gmail.com>
2022-09-01 09:17:41 -0300

    Auto stash before merge of "main" and "origin/main"

--- groovy/two-fer/src/main/groovy/TwoFer.groovy
+++ groovy/two-fer/src/main/groovy/TwoFer.groovy
@@ -1,9 +1,18 @@
 class TwoFer {
 
     static String twoFer(String name) {
+<<<<<<< Updated upstream
         if(name == null || name.isEmpty()){
             return println('One for you, one for me.')
         }
         return println('One for ' + name + ', one for me.')
     }
 }
+=======
+        if(!name){
+            return "One for you, one for me."
+        }
+        return "One for ${name}, one for me."
+    }
+}
+>>>>>>> Stashed changes
